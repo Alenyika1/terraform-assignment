28820c4d62870d45c36c52d27cfdee412961eb29
Jesse Alenyika <alenyikaj@gmail.com>
2024-01-09 21:51:34 +0100

    Updated lines

--- development/root-module.tf
+++ development/root-module.tf
@@ -1,25 +1,22 @@
-
 module "EC2_Instance" {
-  source  = "../child-modules/EC2-Instance"
-  instance_name = "vm-west1"
-  instance_ami =  "ami-0694d931cee176e7d"
-  ec2_instance_type =  "t2.micro"
-  key_name = "key-pair"
-  key_filename = "/home/ubuntu/.ssh/id_rsa"
-  security-name = "security-west"
-  vpc_id_id = module.network.vpc_id_id
+  source            = "../child-modules/EC2-Instance"
+  instance_name     = "vm-west1"
+  instance_ami      = "ami-0694d931cee176e7d"
+  ec2_instance_type = "t2.micro"
+  key_name          = "key-pair"
+  key_filename      = "/home/ubuntu/.ssh/id_rsa"
+  security_name     = "security-west"
+  vpc_id_id         = module.network.vpc_id_id
   private_subnet_id = module.network.private_subnet_id
-  public_subnet_id = module.network.public_subnet_id
-  environment = "dev"
-
+  public_subnet_id  = module.network.public_subnet_id
+  environment       = "dev"
 }
 
-
 module "network" {
-  source = "../child-modules/network"
-  environment = "dev"
-  vpc_name = "VPC-west"
-  cidr_block = "192.56.0.0/16"
+  source        = "../child-modules/network"
+  environment   = "dev"
+  vpc_name      = "VPC-west"
+  cidr_block    = "192.56.0.0/16"
   public_subnet = "192.56.1.0/24"
   private_subnet = "192.56.2.0/24"
 }
--- production/root-modules.tf
+++ production/root-modules.tf
@@ -1,27 +1,22 @@
-
 module "EC2_Instance" {
-  source  = "../child-modules/EC2-Instance"
-  
- instance_name = "VM-2"
- instance_ami = "ami-06dd92ecc74fdfb36"
- ec2_instance_type = "t2.micro"
- key_name = "mykeypair"
- key_filename = "/home/ubuntu/.ssh/id_rsa"
- security-name = "security-central"
- vpc_id_id = module.network.vpc_id_id
- private_subnet_id = module.network.private_subnet_id
- public_subnet_id = module.network.public_subnet_id
- environment = "prod"
+  source            = "../child-modules/EC2-Instance"
+  instance_name     = "VM-2"
+  instance_ami      = "ami-06dd92ecc74fdfb36"
+  ec2_instance_type = "t2.micro"
+  key_name          = "mykeypair"
+  key_filename      = "/home/ubuntu/.ssh/id_rsa"
+  security_name     = "security-cent"
+  vpc_id_id         = module.network.vpc_id_id
+  private_subnet_id = module.network.private_subnet_id
+  public_subnet_id  = module.network.public_subnet_id
+  environment       = "prod"
 }
- 
-
-
 
 module "network" {
-  source = "../child-modules/network"
-  environment = "prod"
-  cidr_block = "10.58.0.0/16"
-  vpc_name = "VPC-Central"
-  public_subnet = "10.58.1.0/24"
-  private_subnet = "10.58.2.0/24"
+  source        = "../child-modules/network"
+  environment   = "prod"
+  cidr_block    = "10.56.0.0/16"
+  vpc_name      = "VPC-Central"
+  public_subnet = "10.56.1.0/24"
+  private_subnet = "10.56.2.0/24"
 }
